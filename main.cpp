93bc04a767433b789997d6be308192ad85510207
taradactyl27 <ataradachuk@gmail.com>
2018-09-17 10:52:52 -0400

    updated main routine

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 
 int main(){
 
+  
   std::string input;
-
+  std::cout << "Enter a word to piglatinify: " << std::endl;
+  std::cin >> input;
+  std::cout << "The word becomes: " << piglatinify(input) << std::endl;
   
 
   return 0;
